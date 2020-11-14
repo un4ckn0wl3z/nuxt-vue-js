cc2ed6246a5e53d81dd962070c37e63c130f4f66
un4ckn0wl3z <slickerth@gmail.com>
2020-11-14 22:47:01 +0700

    add ep1

---
+++ ep1/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span v-if="canSee"> you can see me</span>
+    <span v-else> you cannot see me</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      canSee: false,
+    }
+  },
+}
+</script>
+
+<style></style>
