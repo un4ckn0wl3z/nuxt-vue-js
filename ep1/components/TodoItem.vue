354cf6e5b70fe5e06451f1932ed0a7dad2694e8b
un4ckn0wl3z <slickerth@gmail.com>
2020-11-15 13:04:30 +0700

    play with components

---
+++ ep1/components/TodoItem.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <li>{{ item.text }}</li>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+}
+</script>
+
+<style lang="css" scoped></style>
--- ep1/pages/index.vue
+++ ep1/pages/index.vue
@@ -1,16 +1,28 @@
 <template>
   <div>
-    <span>{{ message }}</span>
-    <br />
-    <input type="text" name="" value="" v-model="message" />
+    <ol>
+      <TodoItem
+        v-for="(todo, index) in todos"
+        :key="index"
+        :item="todo"
+      ></TodoItem>
+    </ol>
   </div>
 </template>
 
 <script>
+import TodoItem from '@/components/TodoItem'
 export default {
+  components: {
+    TodoItem,
+  },
   data() {
     return {
-      message: '',
+      todos: [
+        { id: 1, text: 'Learn JavaScript' },
+        { id: 2, text: 'Learn Vue' },
+        { id: 3, text: 'Build something awesome' },
+      ],
     }
   },
 }
