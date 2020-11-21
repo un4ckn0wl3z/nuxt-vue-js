7cee26b9aea03d6fb3c928e12194562fcfe17649
un4ckn0wl3z <slickerth@gmail.com>
2020-11-21 21:57:21 +0700

    play with states and components

---
+++ vuetify-layout-example/components/Global/Topbar.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <v-app-bar fixed app>
+    <v-app-bar-nav-icon @click.stop="toggleDrawer" />
+    <v-toolbar-title v-text="title" />
+    <v-spacer />
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Vuetify.js',
+    }
+  },
+  methods: {
+    toggleDrawer() {
+      this.$store.commit('set_drawer', !this.$store.state.drawer)
+    },
+  },
+}
+</script>
+
+<style lang="css" scoped></style>
---
+++ vuetify-layout-example/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app dark>
+    <Sidebar></Sidebar>
+    <Topbar></Topbar>
+
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import Sidebar from '@/Components/Global/Sidebar'
+import Topbar from '@/Components/Global/Topbar'
+
+export default {
+  components: {
+    Sidebar,
+    Topbar,
+  },
+}
+</script>
